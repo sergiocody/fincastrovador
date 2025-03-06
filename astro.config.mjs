937b450b0a0e00c7e8c67c69709514a03dee1e97
Sergio Codonal <sergiocody@yahoo.es>
2025-03-06 15:31:13 +0100

    https://fincastrov.netlify.app/

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
 export default defineConfig({
-  site: "https://www.yourwebsite.com", // update me!
+  site: "https://fincastrov.netlify.app/", // update me!
   integrations: [
     icon(),
     sitemap({
